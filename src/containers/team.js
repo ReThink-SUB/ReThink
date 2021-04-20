dcbbb17645d8f2810c055664e94017eb9ad9ef51
Kayla Lee <45502276+kaylalee44@users.noreply.github.com>
2021-04-19 19:49:50 -0700

    worked on some styling for team page

--- src/containers/team.js
+++ src/containers/team.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Team from "../components/team/index";
+import "../components/team/styles/team.css";
 
-export default function TeamContainer(props) {
+export function TeamContainer(props) {
     return (
         <div>
-            <h1>Meet the SUB Team</h1>
-            <p>A ragtag team of UW students deciding to give a damn about the environment, in the most enjoyable way possible.</p>
+            <h1 className="title">SUB Team</h1>
+            <p className="title-desc">A ragtag team of UW students deciding to give a damn about the environment, in the most enjoyable way possible.</p>
             <Team profiles={props.profiles}/>
         </div>
     )
