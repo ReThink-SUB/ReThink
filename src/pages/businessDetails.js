1530b11aae72676335b0249de3c4d838497a0b8f
Audrey Kho <audreyckho@gmail.com>
2021-10-13 19:47:16 -0700

    fixed details page errors

--- src/pages/businessDetails.js
+++ src/pages/businessDetails.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { NavbarContainer } from "../containers";
 import Details from "../components/businessDetails";
 import Footer from "../components/footer/index";
