426c9700ee77b0930ddd0011cf9b07558cf1e736
Audrey Kho <audreyckho@gmail.com>
2021-12-27 01:42:16 -0800

    remove unnecessary data

--- src/pages/businessDetails.js
+++ src/pages/businessDetails.js
@@ -4,27 +4,10 @@ import Details from "../components/businessDetails";
 import Footer from "../components/footer/index";
 
 function DetailPage() {
-  let impact = new Map();
-  let cats = [
-    "Plant-based ingredients",
-    "Compost packaging",
-    "Green supply chain",
-    "Low store footprint",
-  ];
-  let desc = [
-    "Your business should incorporate all technology neccessary to use as less electricity as possible in your establishment. Lowering your energy consumption not only is sustainable, but also",
-    "Your business should incorporate all technology neccessary to use as less electricity as possible in your establishment. Lowering your energy consumption not only is sustainable, but also",
-    "Your business should incorporate all technology neccessary to use as less electricity as possible in your establishment. Lowering your energy consumption not only is sustainable, but also",
-    "Your business should incorporate all technology neccessary to use as less electricity as possible in your establishment. Lowering your energy consumption not only is sustainable, but also",
-  ];
-  cats.forEach((cat, index) => {
-    impact.set(cat, desc[index]);
-  });
-
   return (
     <>
       <NavbarContainer />
-      <Details cards={impact} />
+      <Details />
       <Footer />
     </>
   );
