b86fe932c241bd3a5ba7bf76f0c9e33eb95df2c2
Shawn Lee <sylee2k@cs.washington.edu>
2022-01-04 14:38:31 -0800

    update go to top button styling

--- src/components/scrollToTop/styles/scrollToTop.js
+++ src/components/scrollToTop/styles/scrollToTop.js
@@ -10,24 +10,26 @@ export const Button = styled.div`
     opacity: 0.5;
   button {
     width: 3rem;
-    height: 41px;
+    height: 3rem;
     outline: none;
     border: none;
-    background: #86b390;
+    background: #67923d;
     box-shadow: 0px 30px 60px rgba(57, 57, 57, 0.1);
-    border-radius: 30px;
+    border-radius: 60px;
     cursor: pointer;
     backgroun-color: #ffffff;
     margin-bottom: 10%;
     transition: 0.5s ease all;
     margin: 0 auto;
-
-    &:hover {
-      background: #00cc92;
-    }
   }
 
-  @media (min-width: 1300px) {
+  img {
+    height: auto;
+    width: 1.5rem;  
+  }
+
+  @media (min-width: 1200px) {
+    display: none;
     width: 55%;
     justify-content: flex-end;
   }
