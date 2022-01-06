fd7813676d09ef134dee95ac1c0e03072160b9d4
Israel Martinez <israel.r.mar@gmail.com>
2022-01-06 02:08:26 -0800

    cleaned up comments and prints

--- src/components/criteria/index.js
+++ src/components/criteria/index.js
@@ -1,12 +1,6 @@
-import React, {useEffect, useState, useRef } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Dimensions } from 'react-native';
-import { makeStyles } from "@material-ui/core/styles";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { db, storage } from "../../firebase";
+import { db } from "../../firebase";
 import "./styles/criteria.scss";
 import {
   CritContainer,
@@ -21,108 +15,24 @@ import {
 const windowWidth = Dimensions.get('window').width;
 
 export default function Container() {
-  let one = [
-    "Natural",
-    "USDA Organic",
-    "Fair Trade",
-    "Certified Humane",
-    "Food Alliance Certification",
-    "Non-GMO",
-    "Local",
-    "Rainforest Alliance Certified",
-    "Certified Animal Welfare Approved",
-  ];
-  let two = [
-    "No GMO",
-    "Provides vegetarian and vegan options",
-    "No animal testing",
-    "Implements ethical farming practices",
-    "Sources ingredients from Pacific Northwest farms (Washington, Oregon, Idaho)",
-    "Buy from regional farms",
-    "Little to no perservatives on farm",
-    "No pesticides",
-    "Uses certified organic products",
-    "Farm certifications: Certified Humane, Animal Welfare Approved, American Grass-fed, American Humane Certified, Global Animal Partnership 4, 5, and 5+",
-  ];
-  let three = [
-    "50% of applicances are certified by Energy Star (refrigerators, microwaves, dishwasher, HVAC, etc)",
-    "Uses LED lights",
-    "Automatic lights",
-    "Uses environmentally-friendly cleaning products",
-    "Touchless sensor faucets",
-    "Dual flush toilet system",
-    "Building is RainWater certified",
-    "Building reuses greywater for irrigation/plumbing",
-    "Check box for using rented spaces --> we understand small businesses aren't able to do all of these",
-  ];
-  let four = [
-    "Integrates compost and recycling bins",
-    "Uses compostable and/or reusable supplies (ex. utensils, cups, napkins, trays, glasses, seat covers, etc.)",
-    "Uses compostable and/or recyclable packaging",
-    "Practices waste reduction in the office (supplies, paperless)",
-    "Donates extra and leftover food and/or products",
-    "Has pictures of what goes where for trash sorting",
-    "Provides reusable dishes for inperson dining",
-    "Donates or sells imperfect ingredients",
-  ];
-  let five = [
-    "Posts accurate info on social media accounts and websites",
-    "Educates staff about sustainability and sustainable business practices",
-    "Charitable goods for community",
-    "Transparency in practices",
-    "Actively attempts to increase and support environmental justices and campaigns",
-    "Overall supports community and is transparent with them",
-  ];
-  let six = [
-    "Good working conditions",
-    "Family-owned",
-    "Minority-owned",
-    "BIPOC-owned",
-    "Woman-owned",
-    "Disabled-owned",
-  ];
-  let categories = {
-    "Certifications": one,
-    "Sustainable Ingredients and Foods": two,
-    "Resource Management": three,
-    "Waste Management": four,
-    "Beyond the Business": five,
-    "Other Badges": six
-  };
-  const [industries, setIndustries] = useState([]);
   const [width, setWidth] = useState(windowWidth);
-  const [industris, setIndustris] = useState([]);
+  const [industries, setIndustries] = useState([]);
   const [active, setActive] = useState(null);
   const [criteria, setCriteria] = useState({});
 
   useEffect(() => {
     let industryHolder = [];
     db.collection('criteria').get().then((querySnapshot) => {
-      let i = 0;
       querySnapshot.forEach((doc) => {
-        console.log(i);
-        industryHolder.push(<Industri data={doc.data()} value={doc.id} setActive={setActive} setCriteria={setCriteria}/>);
-        i++;
+        industryHolder.push(<Industry data={doc.data()} value={doc.id} setActive={setActive} setCriteria={setCriteria}/>);
       });
-      generateIndustris(industryHolder);
+      setIndustries(industryHolder);
     });
     Dimensions.addEventListener(
       "change", (window => {
       setWidth(window['window']['width']);
     }));
   }, []);
-
-  const generateIndustris = (industrys) => {
-    setIndustris(industrys);
-  }
-
-  let filters = Object.entries(categories).map(([key, value]) => {
-    return <Selection category={key} list={industries} setList={setIndustries}/>;
-  });
-
-  let criteriaList = industries.map((industry) => {
-    return <CriteriaCard industry={industry} desc={categories[industry]}/>
-  });
 
   return <CritContainer className="about-container">
       <Main className="about-main">
@@ -144,45 +54,25 @@ export default function Container() {
           </Text>
         </CriteriaSec>
         <div className='line-border'/>
-        {/* <CriteriaSec className="desc-sec">
-          <Text>
-            <Heading>
-              <span style={{color: "#67923D"}}>{criteria.title}</span>
-            </Heading>
-            <SubDescription>
-            {criteria.desc}
-            </SubDescription>
-            <h1>{criteria.title}</h1>
-            <p>{criteria.desc}</p>
-          </Text>
-        </CriteriaSec> */}
         <div className="description">
           <h1>{criteria.title}</h1>
           <p>{criteria.desc}</p>
         </div>
         <div className="filter-selections">
-          {industris}
+          {industries}
         </div>
         <div className="criteria-section">
           {active}
         </div>
-        {/* <div className="filter-selections">
-          {filters}
-        </div>
-        <div className="criteria-section">
-          {criteriaList}
-        </div> */}
       </Main>
     </CritContainer>
   ;
 }
 
-function Industri ({data, value, setActive, setCriteria}) {
-  // let data = props.data;
+function Industry ({data, value, setActive, setCriteria}) {
   const [open, setOpen] = useState(false);
   let desc = data.industry_description;
   let criterias = data.criteria_boxes;
-  // console.log(criterias, 'crit');
 
   useEffect(() => {
     const clickHandler = ({ target }) => {
@@ -196,123 +86,27 @@ function Industri ({data, value, setActive, setCriteria}) {
   })
 
   const handleClick = () => {
-    setOpen(!open);
-    console.log(value, desc, 'desc');
-    
-    // let newList = criterias.map((crit) => {
-    //   return <CritCard title={crit.title} desc={crit.desc}/>
-    // });
-
     let newList = Object.entries(criterias).map(([key, value]) => {
-      return <CritCard title={value.title} desc={value.desc}/>;
+      return <CriteriaCard title={value.title} desc={value.desc}/>;
     });
-    console.log(newList);
-    setActive(newList);
-    setCriteria({'title': value, 'desc': desc});
+    setOpen(!open);
+    if (!open == true) {
+      setActive(newList);
+      setCriteria({'title': value, 'desc': desc});
+    } else {
+      setActive(null);
+      setCriteria({});
+    }
   }
 
   return <button id={value} className={"filter-select" + (open === true ? " selected" : "")} onClick={handleClick}>{value}</button>;
 }
 
-function CritCard ({title, desc}) {
+function CriteriaCard ({title, desc}) {
   return (
     <div className="industry-card">
       <h2 className="industry-title">{title}</h2><br/>
       <p className='industry-criteria'>{desc}</p>
-    </div>
-  );
-}
-
-function Selection ({category, list, setList}) {
-  const [selected, setSelected] = useState(false);
-
-  const changeList = () => {
-    setSelected(prevSelect => !prevSelect);
-    let newList = [...list];
-    let i = 0;
-    let itemInList = false;
-    while (!itemInList && i < list.length) {
-      itemInList = list[i] === category;
-      i++;
-    }
-    if (!itemInList) {
-      newList.unshift(category);
-    }
-    
-    if (itemInList && selected) {
-      newList = newList.filter((item) => item !== category);
-    }
-    setList(newList);
-  }
-  
-  return <button className={"filter-select" + (selected ? " selected" : "")} onClick={changeList}>{category}</button>;
-}
-
-function CriteriaCard ({industry, desc}) {
-  return (
-    <div className="industry-card">
-      <h2 className="industry-title">{industry}</h2><br/>
-      <p>Criteria:</p>
-      <div className='industry-criteria'>
-        {desc.map((point) => {
-          return <Bullet text={point} />;
-        })}
-      </div>
-    </div>
-  );
-}
-
-function Bullet(props) {
-  return (
-    <p>
-      <i className="fa fa-check-circle-o" aria-hidden="true" />
-      &nbsp;{props.text}
-    </p>
-  );
-}
-
-
-
-// Old criteria selection
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginBottom: ".4em",
-    width: "40em",
-  },
-  summary: {
-    backgroundColor: "#A9D0A9",
-    borderRadius: "10px",
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(20),
-    fontWeight: theme.typography.fontWeightMedium,
-    marginLeft: ".2em",
-    color: "#535461",
-  },
-}));
-
-export function CriteriaCat(props) {
-  const classes = useStyles();
-  let arr = props.content;
-
-  return (
-    <div className={classes.root}>
-      <Accordion>
-        <AccordionSummary
-          className={classes.summary}
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel-content"
-        >
-          <Typography className={classes.heading}>{props.title}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div>
-            {arr.map((point) => {
-              return <Bullet text={point} />;
-            })}
-          </div>
-        </AccordionDetails>
-      </Accordion>
     </div>
   );
 }
