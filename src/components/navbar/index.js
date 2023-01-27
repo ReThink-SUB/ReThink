1b33157d64e032ff07aae7a606ec9591101c608b
jeremynguyen593 <jeremynugyen593@gmail.com>
2023-01-26 20:32:08 -0800

    Changed the logo by added the new png to the images file and redirecting the image link for logo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -17,7 +17,7 @@ Navbar.Title = function NavbarTitle({ to, children, ...restProps }) {
           className="mobile-logo"
         />
         <img
-          src="/images/dark_rethink_horizontal_transparent_logo.png"
+          src="/images/ReThink-Logo-Winter-23.png"
           alt="Logo"
           className="desktop-logo"
         />
